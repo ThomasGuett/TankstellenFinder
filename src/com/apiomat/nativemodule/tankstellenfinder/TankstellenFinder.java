4c7d23ad07448580b8446621b5836b105a2a047a
Thomas Guett <guett@apiomat.com>
2018-10-29 10:57:34 +0100

    [AOM] Change code for module TankstellenFinder in system LIVE

--- src/com/apiomat/nativemodule/tankstellenfinder/TankstellenFinder.java
+++ src/com/apiomat/nativemodule/tankstellenfinder/TankstellenFinder.java
@@ -35,7 +35,7 @@ import com.apiomat.nativemodule.NativeModuleConfig.Type;
  * 
  */
 @com.apiomat.nativemodule.Module( description = "",
-	usedModules = { }, securityPermissions = { } )
+	usedModules = { "Basics" }, securityPermissions = { } )
 public class TankstellenFinder implements com.apiomat.nativemodule.IModule
 {
 	static com.apiomat.nativemodule.IApplicationConfigProxy APP_CONFIG_PROXY;
